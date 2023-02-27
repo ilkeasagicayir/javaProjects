1fbf84e965679f8a12a8035c3c7f27d734f7c5b1
İlke Aşağıçayır <ilke.asagicayir@gmail.com>
2023-02-27 18:33:33 +0300

    Second lesson

---
+++ patikadev/src/deneme/ders2.java
@@ -0,0 +1,22 @@
+package deneme;
+
+import java.util.Scanner;
+
+public class ders2 {
+	public static void main(String[] args) {
+		double tutar ;
+		Scanner inp = new Scanner(System.in);
+		System.out.print("Ücret tutarını gir : ");
+		tutar = inp.nextDouble();
+
+		double kdvOran = (tutar >= 0 && tutar<=1000) ? 0.18 : 0.08;
+		double kdvTutar = tutar*kdvOran ; 
+		
+		double kdvsiz = tutar , kdvli= (kdvTutar+tutar);
+		System.out.println("KDV ORANI: "+kdvOran);
+		System.out.println("KDV TUTAR: "+kdvTutar);
+		System.out.println("KDVSIZ FIYAT : "+kdvsiz);
+		System.out.println("KDVLİ FIYAT : "+kdvli);
+		}
+
+}
