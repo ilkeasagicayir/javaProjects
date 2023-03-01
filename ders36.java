c7be99c2d4059e54e9d3c68418d9149a2b59c735
İlke Aşağıçayır <79101412+ilkeasagicayir@users.noreply.github.com>
2023-03-01 22:54:48 +0300

    Fibonacci Serisi

---
+++ ders36.java
@@ -0,0 +1,23 @@
+package deneme;
+
+import java.util.Scanner;
+
+public class ders36 {
+	public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int element, first = 0, second = 1;
+
+        System.out.print("Fibonacci serisinin eleman sayısını giriniz: ");
+        element = input.nextInt();
+
+        for (int i = 1; i <= element + 1; i++) {
+            System.out.print(first + " ");
+            int total = first + second;
+            first = second;
+            second = total;
+        }
+	}
+
+}
